01cc1b7b149667645f16a5528fb591a1c094914d
Willy7g <williamgon32@gmail.com>
2023-08-15 16:20:47 -0600

    v 0.07

--- src/app/Vistas/valida-num/valida-num.component.ts
+++ src/app/Vistas/valida-num/valida-num.component.ts
@@ -10,13 +10,33 @@ export class ValidaNumComponent implements OnInit {
 
   inputText: string="";
   message: string="";
+  numeroEntero: number=0;
+  primerosCuatroDigitos: string="";
+
 
   extractAndCheck() {
+    console.log("entro")
+
+
+
     const primeroDosNumeros = this.inputText.substr(0, 2);
+    const primeroCuatroNumeros =  this.inputText.substr(0, 4);
     
-    if (+primeroDosNumeros == 30 || +primeroDosNumeros==31 ||+primeroDosNumeros==40 ) {
+ 
+    const primerosCuatroCaracteres = this.primerosCuatroDigitos.substring(0, 4);
+
+   // Convertir a número entero
+   this.numeroEntero = parseInt(primerosCuatroCaracteres, 10);
+
+   console.log("variable 4 caracteres " +primerosCuatroCaracteres)
+      console.log("antes del if " +this.numeroEntero)
+
+
+
+
+    if (this.numeroEntero >= 3230 && this.numeroEntero<=3299) {
       this.message = 'TIGO';
-    } else if(+primeroDosNumeros==41 || +primeroDosNumeros==42 ||+primeroDosNumeros==43){
+    } else if(+primeroDosNumeros==41 || +primeroDosNumeros==42 ||+primeroDosNumeros==43 ||+primeroCuatroNumeros==3229){
       
       this.message = 'CLARO';
     }else{
@@ -24,7 +44,7 @@ export class ValidaNumComponent implements OnInit {
     }
   }
 
-
+//cmabios primer parte auqi escribi algo
 
 
   
